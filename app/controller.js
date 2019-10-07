f2d7032fc2ad9ce0e27bb3f61d5c453fef335a80
Henrique Albuquerque <henrique_barretto@outlook.com>
2019-10-07 17:06:18 -0300

    tudo

--- app/controller.js
+++ app/controller.js
@@ -8,10 +8,16 @@ router.get('/', (request, response, next) => {
     let sum = ArduinoData.List.reduce((a, b) => a + b, 0);
     let average = (sum / ArduinoData.List.length).toFixed(2);
 
+    let sumB = ArduinoData.ListB.reduce((a, b) => a + b, 0);
+    let averageB = (sumB / ArduinoData.ListB.length).toFixed(1);
+
     response.json({
         data: ArduinoData.List,
+        dataB: ArduinoData.ListB,
         total: ArduinoData.List.length,
-        average: isNaN(average) ? 0 : average
+        totalB: ArduinoData.ListB.length,
+        average: isNaN(average) ? 0 : average,
+        averageB: isNaN(averageB) ? 0 : averageB
     });
 
 });
--- app/serial.js
+++ app/serial.js
@@ -3,46 +3,56 @@ const Readline = SerialPort.parsers.Readline;
 
 class ArduinoDataRead {
 
-    constructor(){
+    constructor() {
         this.listData = [];
+        this.listDataB = [];
     }
 
     get List() {
         return this.listData;
     }
 
-    SetConnection(){
+    get ListB() {
+        return this.listDataB;
+    }
+
+    SetConnection() {
 
         SerialPort.list().then(listSerialDevices => {
-            
+
             let listArduinoSerial = listSerialDevices.filter(serialDevice => {
                 return serialDevice.vendorId == 2341 && serialDevice.productId == 43;
             });
-            
-            if (listArduinoSerial.length != 1){
+
+            if (listArduinoSerial.length != 1) {
                 throw new Error("The Arduino was not connected or has many boards connceted");
             }
 
             console.log("Arduino found in the com %s", listArduinoSerial[0].comName);
-             
-            return  listArduinoSerial[0].comName;
-            
+
+            return listArduinoSerial[0].comName;
+
         }).then(arduinoCom => {
-            
-            let arduino = new SerialPort(arduinoCom, {baudRate: 9600});
-            
+
+            let arduino = new SerialPort(arduinoCom, { baudRate: 9600 });
+
             const parser = new Readline();
             arduino.pipe(parser);
-            
+
             parser.on('data', (data) => {
-                this.listData.push(parseFloat(data));
+                var result = data.split(";");
+                this.listData.push(parseFloat(result[0]));
+                this.listDataB.push(parseFloat(result[1]));
             });
-            
+
         }).catch(error => console.log(error));
-    } 
+    }
 }
 
 const serial = new ArduinoDataRead();
 serial.SetConnection();
 
-module.exports.ArduinoData = {List: serial.List} 
+module.exports.ArduinoData = {
+    List: serial.List,
+    ListB: serial.ListB
+}; 
